a26bd7e87fd845975fce391ea3a35d9837bdbf9f
Pranav Vashi <neobuddy89@gmail.com>
2016-09-21 01:38:40 +0530

    cpufreq: impulse: Add gfx boost mode
    Change-Id: I86cf740c62eed306f4cd5f8de80ba678813f9cab
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- include/linux/msm_kgsl.h
+++ include/linux/msm_kgsl.h
@@ -21,6 +21,10 @@
 #define KGSL_3D0_SHADER_MEMORY	"kgsl_3d0_shader_memory"
 #define KGSL_3D0_IRQ		"kgsl_3d0_irq"
 
+#ifdef CONFIG_CPU_FREQ_GOV_IMPULSE
+extern int gfx_level;
+#endif
+
 /**
  * struct kgsl_pwrlevel - Struct holding different pwrlevel info obtained from
  * from dtsi file
